7056e15d3449dcb355e1fb4c1af75e8b0b9a7773
Dajan Plackovic <dplackov@c3r5p5.42vienna.com>
2024-09-27 16:06:52 +0200

    norminette passing hackjob

--- get_next_line.c
+++ get_next_line.c
@@ -11,21 +11,15 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <sys/types.h>
 #include <unistd.h>
 
-char	*get_next_line(int fd)
+char	*handle_line(int fd, char *text, char **buff)
 {
-	char		*text;
-	static char	*buff = 0;
-	ssize_t		bread;
-	char		*pos;
-	char		*out;
+	ssize_t	bread;
+	char	*pos;
+	char	*out;
 
-	if (fd < 0 || read(fd, buff, 0) == -1)
-		return (NULL);
-	if (!buff)
-		buff = (char *)ft_calloc(1, 1);
-	text = ft_strdup(buff);
 	bread = 1;
 	while (bread > 0)
 	{
@@ -33,18 +27,31 @@ char	*get_next_line(int fd)
 		if (pos)
 		{
 			out = ft_substr(text, 0, pos - text + 1);
-			buff = ft_substr(text, pos - text + 1, ft_strlen(text) - (pos - text
-						+ 1));
+			*buff = ft_substr(text, pos - text + 1, ft_strlen(text) - (pos
+						- text + 1));
 			free(text);
 			return (out);
 		}
 		else
 		{
-			bread = read(fd, buff, BUFFER_SIZE);
-			text = ft_strjoin(text, buff);
+			bread = read(fd, *buff, BUFFER_SIZE);
+			text = ft_strjoin(text, *buff);
 		}
 	}
 	free(text);
-	free(buff);
+	free(*buff);
 	return (NULL);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*text;
+	static char	*buff = 0;
+
+	if (fd < 0 || read(fd, buff, 0) == -1)
+		return (NULL);
+	if (!buff)
+		buff = (char *)ft_calloc(1, 1);
+	text = ft_strdup(buff);
+	return (handle_line(fd, text, &buff));
 }
