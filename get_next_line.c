7ed6d6d45e60a122dd56a6ffaca536ea729650f5
Dajan Plackovic <dplackov@c3r5p5.42vienna.com>
2024-09-27 15:46:28 +0200

    works for simple cases with full libft

--- get_next_line.c
+++ get_next_line.c
@@ -10,7 +10,38 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char *get_next_line(int fd)
-{
+#include "libft.h"
+#include <unistd.h>
 
+char	*get_next_line(int fd)
+{
+	char			*text;
+	static char		*buff = 0;
+	static ssize_t	bread = 1;
+	char			*pos;
+	char			*out;
+
+	if (!buff)
+		buff = (char *)ft_calloc(1, 1);
+	text = ft_strdup(buff);
+	while (bread > 0)
+	{
+		pos = ft_strchr(text, '\n');
+		if (pos)
+		{
+			out = ft_substr(text, 0, pos - text + 1);
+			buff = ft_substr(text, pos - text + 1, ft_strlen(text) - (pos - text
+						+ 1));
+			free(text);
+			return (out);
+		}
+		else
+		{
+			bread = read(fd, buff, BUFFER_SIZE);
+			text = ft_strjoin(text, buff);
+		}
+	}
+	free(text);
+	free(buff);
+	return (NULL);
 }
