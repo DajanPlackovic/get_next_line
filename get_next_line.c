13899054b990c3e6d49308304ada439c29a48d40
Dajan Plackovic <plackovicdajan@gmail.com>
2024-09-27 20:18:15 +0200

    add substring prototype to main file

--- get_next_line.c
+++ get_next_line.c
@@ -18,6 +18,7 @@ size_t	ft_strlen(const char *s);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strchr(const char *s, char c);
 char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
