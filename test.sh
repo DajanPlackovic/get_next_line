bdd9addc1ad369e1a96e230ff1c4353ab450c62c
Dajan Plackovic <plackovicdajan@gmail.com>
2024-09-29 17:29:57 +0200

    more test stuff

--- test.sh
+++ test.sh
@@ -1,12 +1,21 @@
-printf "%s " "NORMINETTE ERRORS\n"
+#!/usr/bin/env bash
+
+echo "NORMINETTE ERRORS"
 norminette get_next_line.c | awk '!/OK!$/'
 norminette get_next_line_utils.c | awk '!/OK!$/'
 norminette get_next_line.h | awk '!/OK!$/'
-printf "%s " "Press enter to continue\n"
+printf "%s " "Press enter to continue"
 read ans
 cc *.c -g -L. -lft
+# if there are no arguments
 if [ $# -eq 0 ]; then
+# run tests with all files
 ./a.out $(ls *.txt)
 else
-./a.out $@
+arr=()
+for var in $@
+do
+arr+=($(echo $var | awk '!/\.txt$/ {print $0".txt"} /\.txt$/ {print $0}'))
+done
+./a.out "${arr[@]}"
 fi
