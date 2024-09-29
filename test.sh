8e5b0a298af7b1ca8c24f419a3b6e8590a8c4e49
Dajan Plackovic <plackovicdajan@gmail.com>
2024-09-29 16:59:27 +0200

    another test improvement

--- test.sh
+++ test.sh
@@ -1,5 +1,12 @@
+printf "%s " "NORMINETTE ERRORS\n"
 norminette get_next_line.c | awk '!/OK!$/'
 norminette get_next_line_utils.c | awk '!/OK!$/'
 norminette get_next_line.h | awk '!/OK!$/'
+printf "%s " "Press enter to continue\n"
+read ans
 cc *.c -g -L. -lft
+if [ $# -eq 0 ]; then
+./a.out $(ls *.txt)
+else
 ./a.out $@
+fi
